d3010c78ee26bdba906f94e7f22198a30c6aa9ba
shivammaniharsahu <shivam.raipur1234@gmail.com>
2020-01-07 20:44:33 +0530

    React redux

---
+++ todoapps/src/components/App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+
+
+
+
+export default class App extends Component
+{
+  
+    render()
+    {
+    
+        return(
+          <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+        )
+    }
+}
